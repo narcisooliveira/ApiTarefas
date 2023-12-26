a2e71e9b33728f95717d100ef818bbf337c149e3
narcisooliveira <nrcsfo@gmail.com>
2023-12-26 20:56:28 -0300

    Criando modelo de domínio e endpoints para criação, leituras, alteração e deleção de tarefas.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TarefasContext>(options =>
+{
+    options.UseInMemoryDatabase("Tarefas");
+});
 
 var app = builder.Build();
 
@@ -14,9 +23,82 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/phases", async () =>
+app.MapGet("/tarefas", async (TarefasContext db) =>
 {
-    await new HttpClient().GetAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes");
+    return await db.Tarefas.ToListAsync();
+});
+
+app.MapGet("/tarefas/{id}", async (TarefasContext db, int id) =>
+{
+    return await db.Tarefas.FindAsync(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound();
+});
+
+app.MapGet("/tarefas/concluidas", async (TarefasContext db) =>
+{
+    return await db.Tarefas.Where(t => t.Concluida).ToListAsync();
+});
+
+app.MapPost("/tarefas", async (TarefasContext db, Tarefa tarefa) =>
+{
+    await db.Tarefas.AddAsync(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
+});
+
+app.MapPut("/tarefas/{id}", async (TarefasContext db, int id, Tarefa tarefa) =>
+{
+    if (id != tarefa.Id)
+    {
+        return Results.BadRequest();
+    }
+
+    var tarefaExistente = await db.Tarefas.FindAsync(id);
+    if (tarefaExistente is null)
+    {
+        return Results.NotFound();
+    }
+
+    tarefaExistente.Titulo = tarefa.Titulo;
+    tarefaExistente.Descricao = tarefa.Descricao;
+    tarefaExistente.DataConclusao = tarefa.DataConclusao;
+    tarefaExistente.Concluida = tarefa.Concluida;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/tarefas/{id}", async (TarefasContext db, int id) =>
+{
+    var tarefa = await db.Tarefas.FindAsync(id);
+    if (tarefa is null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Tarefas.Remove(tarefa);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
 });
 
 app.Run();
+
+class Tarefa
+{
+    [Key]
+    public int Id { get; set; }
+    public string? Titulo { get; set; }
+    public string? Descricao { get; set; }
+    public DateTime DataCriacao { get; set; } = DateTime.Now;
+    public DateTime DataConclusao { get; set; } = DateTime.Now.AddDays(7);
+    public bool Concluida { get; set; } = false;
+}
+
+class TarefasContext : DbContext
+{
+    public TarefasContext(DbContextOptions<TarefasContext> options) : base(options) { }
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+
+}
